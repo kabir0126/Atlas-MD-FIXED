f72e758445e05d0078f2a8303827f4913bee371d
kabir0126 <kabirwithshriya@gmail.com>
2025-01-23 00:11:38 +0530

    Update Configurations.js

--- Configurations.js
+++ Configurations.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 
 let gg = process.env.MODS;
 if (!gg) {
-  gg = "918101187835";   // You can replace this number with yours //
+  gg = "918252219860";   // You can replace this number with yours //
 }
 
 
 global.owner = gg.split(",");
-global.mongodb = process.env.MONGODB || "Put your alternative mongodb url here";
+global.mongodb = process.env.MONGODB || "mongodb+srv://Atlas_MD:<Komal1539A>@cluster0.poiwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 global.sessionId = process.env.SESSION_ID || "ok";
 global.prefa = process.env.PREFIX || "-";
 global.tenorApiKey = process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
